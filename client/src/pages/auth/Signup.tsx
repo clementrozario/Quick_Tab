a6a82411656cf5d04dfa8c1a0172b8a10062d23a
Clement Delphin <clementcla7@gmail.com>
2026-01-13 21:01:16 +0530

    feat(auth): build signup page ui

--- client/src/pages/auth/Signup.tsx
+++ client/src/pages/auth/Signup.tsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom"
+import { PiInvoiceDuotone } from "react-icons/pi";
 
 export const Signup = () => {
     return (
-        <div>Signup page</div>
+        <div className="min-h-screen flex justify-center items-baseline pt-12 bg-gray-100">
+            <div className="bg-white p-10 rounded-lg shadow-lg w-112.5">
+                <div className="flex justify-center items-center gap-2">
+                    <PiInvoiceDuotone className="text-2xl"/>
+                    <h1 className="text-xl font-bold text-center"> Quick Tab</h1>
+                </div>
+
+                <h2 className="text-2xl font-semibold text-center mt-8">Create an Account</h2>
+                
+                <div className="mt-8">
+                    <label htmlFor="email" className="text-sm font-medium">Email</label>
+                    <input type="email" className="w-full border border-gray-300 rounded px-3 py-2 mt-1" />
+                </div>
+
+                <div className="mt-6">
+                    <label htmlFor="password" className="text-sm font-medium">Password</label>
+                    <input type="password" className="w-full border border-gray-300 rounded px-3 py-2 mt-1" />
+                </div>
+
+                <button className="w-full bg-black text-white py-4 mt-6 rounded-xl font-semibold hover:bg-gray-800 cursor-pointer">
+                    Sign up
+                </button>
+
+                <p className="text-center mt-4">Already have an account?
+                    <Link to='/login' className="text-blue-600"> Login</Link>
+                </p>
+            </div>
+        </div>
     )
 }
 
