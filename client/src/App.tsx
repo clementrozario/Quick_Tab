975d5318ef4529993bc0e05068a55a558513e852
Clement Delphin <clementcla7@gmail.com>
2026-01-13 17:34:49 +0530

    feat(routing):implement React Router with auth & landing pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./pages/auth/Login";
+import { Signup } from "./pages/auth/Signup";
+import { LandingPage } from "./pages/landingPage";
+
 
 function App() {
-  const [count, setCount] = useState<number>(0)
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-xl shadow-lg text-center max-w-md">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">Quick Tab</h1>
-        <p className="text-lg text-gray-700 mb-4">welcome to the quick tab we will get invoice</p>
-        <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg transition-duration-300"
-          onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+    </Routes>
+
   )
 }
 
---
+++ client/src/pages/landingPage.tsx
@@ -0,0 +1,8 @@
+
+
+export const LandingPage = () => {
+    return (
+        <div className="font-bold to-blue-200 text-4xl">Landing page</div>
+    )
+}
+
