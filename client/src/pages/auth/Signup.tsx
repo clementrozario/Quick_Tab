41767c72ecfe946899f93d74f98b049709e20667
Clement Delphin <clementcla7@gmail.com>
2026-01-14 11:02:32 +0530

    feat(auth): integrate React Hook Form for signup validation

--- client/src/pages/auth/Signup.tsx
+++ client/src/pages/auth/Signup.tsx
@@ -1,7 +1,19 @@
 import { Link } from "react-router-dom"
 import { PiInvoiceDuotone } from "react-icons/pi";
+import { useForm } from 'react-hook-form'
+
+type SignupFormData = {
+    email: string,
+    password:string
+}
 
 export const Signup = () => {
+    const { register, handleSubmit } = useForm<SignupFormData>()
+    
+    const onSubmit = (data:SignupFormData) => {
+        console.log(data)
+    }
+    
     return (
         <div className="min-h-screen flex justify-center items-baseline pt-12 bg-gray-100">
             <div className="bg-white p-10 rounded-lg shadow-lg w-112.5">
@@ -12,19 +24,29 @@ export const Signup = () => {
 
                 <h2 className="text-2xl font-semibold text-center mt-8">Create an Account</h2>
                 
+                <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mt-8">
                     <label htmlFor="email" className="text-sm font-medium">Email</label>
-                    <input type="email" className="w-full border border-gray-300 rounded px-3 py-2 mt-1" />
+                    <input
+                        type="email"
+                        className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                        {...register('email')}
+                    />
                 </div>
 
                 <div className="mt-6">
                     <label htmlFor="password" className="text-sm font-medium">Password</label>
-                    <input type="password" className="w-full border border-gray-300 rounded px-3 py-2 mt-1" />
+                    <input
+                        type="password"
+                        className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                        {...register('password')}
+                    />
                 </div>
 
                 <button className="w-full bg-black text-white py-4 mt-6 rounded-xl font-semibold hover:bg-gray-800 cursor-pointer">
                     Sign up
                 </button>
+                </form>
 
                 <p className="text-center mt-4">Already have an account?
                     <Link to='/login' className="text-blue-600"> Login</Link>
