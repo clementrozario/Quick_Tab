550cde5b7fcaa3a110aad57472e871329602b71f
Clement Delphin <clementcla7@gmail.com>
2026-01-14 20:43:45 +0530

    feat(auth):integrate react hook in login validation

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,7 +1,19 @@
 import { PiInvoiceDuotone } from "react-icons/pi";
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+
+type SignupFormData = {
+    email: string,
+    password:string
+}
 
 export const Login = () => {
+    const { register, handleSubmit } = useForm<SignupFormData>()
+    
+    const onSubmit = (data:SignupFormData) => {
+        console.log(data)
+    }
+    
   return (
       <div className="min-h-screen flex items-baseline justify-center pt-12 bg-gray-100">
           <div className="bg-white p-10 rounded-lg shadow-lg w-112.5">
@@ -13,11 +25,13 @@ export const Login = () => {
               
               <h2 className="mt-8 font-semibold text-2xl text-center">Welcome Back</h2>
 
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="mt-8">
                     <label htmlFor="email" className="text-sm font-medium">Email</label>
                   <input
                       type="text"
                       className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                      {...register('email')}
                   />  
               </div>
 
@@ -26,6 +40,7 @@ export const Login = () => {
                   <input
                       type="password"
                       className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                      {...register('password')}
                   />
               </div>
 
@@ -36,6 +51,7 @@ export const Login = () => {
               <button className="bg-gray-100 w-full px-3 py-2 rounded font-medium mt-2 hover:bg-gray-400 cursor-pointer">
                   Reset Password
               </button>
+            </form>
 
               <p className="mt-4 text-center">Don't have an account?
                   <Link to='/signup' className="text-blue-600 hover:text-blue-400"> Signup</Link>
